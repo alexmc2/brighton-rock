4f6d588989d59cbf8e59ea47ac294c5a60ed16e2
Alex <alex_mcgarry@hotmail.co.uk>
2023-07-16 20:19:02 +0100

    toggle

--- js/script.js
+++ js/script.js
@@ -13,6 +13,7 @@ function userScroll() {
 }
 
 document.addEventListener('DOMContentLoaded', userScroll);
+
 document.getElementById('theme-toggle').addEventListener('click', function () {
   // Toggle the 'dark-theme' class on the body element
   document.body.classList.toggle('dark-theme');
@@ -21,9 +22,15 @@ document.getElementById('theme-toggle').addEventListener('click', function () {
   if (document.body.classList.contains('dark-theme')) {
     // If it does, save 'dark' as the theme in local storage
     localStorage.setItem('theme', 'dark');
+    // Change the icon to 'fa-toggle-on'
+    this.classList.remove('fa-toggle-off');
+    this.classList.add('fa-toggle-on');
   } else {
     // If it doesn't, save 'light' as the theme in local storage
     localStorage.setItem('theme', 'light');
+    // Change the icon to 'fa-toggle-off'
+    this.classList.remove('fa-toggle-on');
+    this.classList.add('fa-toggle-off');
   }
 });
 
@@ -31,13 +38,20 @@ document.getElementById('theme-toggle').addEventListener('click', function () {
 window.addEventListener('DOMContentLoaded', (event) => {
   // Get the saved theme from local storage
   const savedTheme = localStorage.getItem('theme');
+  const themeToggle = document.getElementById('theme-toggle');
 
   // If a theme was saved, and it's 'dark'
   if (savedTheme === 'dark') {
     // Add the 'dark-theme' class to the body
     document.body.classList.add('dark-theme');
+    // Change the icon to 'fa-toggle-on'
+    themeToggle.classList.remove('fa-toggle-off');
+    themeToggle.classList.add('fa-toggle-on');
   } else if (savedTheme === 'light') {
     // If the saved theme is 'light', make sure the 'dark-theme' class is not on the body
     document.body.classList.remove('dark-theme');
+    // Change the icon to 'fa-toggle-off'
+    themeToggle.classList.remove('fa-toggle-on');
+    themeToggle.classList.add('fa-toggle-off');
   }
 });
