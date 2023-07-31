462cd67efb9838d2b0d16f28246ac3a23194f168
Alex <alex_mcgarry@hotmail.co.uk>
2023-07-31 16:14:45 +0100

    update header background

--- js/script.js
+++ js/script.js
@@ -3,14 +3,29 @@ function userScroll() {
 
   window.addEventListener('scroll', () => {
     if (window.scrollY > 50) {
-      navbar.classList.add('bg-dark');
       navbar.classList.add('navbar-sticky');
     } else {
-      navbar.classList.remove('bg-dark');
       navbar.classList.remove('navbar-sticky');
     }
   });
 }
+
+window.onload = userScroll;
+
+
+// function userScroll() {
+//   const navbar = document.querySelector('.navbar-dark');
+
+//   window.addEventListener('scroll', () => {
+//     if (window.scrollY > 50) {
+//       navbar.classList.add('bg-dark');
+//       navbar.classList.add('navbar-sticky');
+//     } else {
+//       navbar.classList.remove('bg-dark');
+//       navbar.classList.remove('navbar-sticky');
+//     }
+//   });
+// }
 
 document.addEventListener('DOMContentLoaded', userScroll);
 
